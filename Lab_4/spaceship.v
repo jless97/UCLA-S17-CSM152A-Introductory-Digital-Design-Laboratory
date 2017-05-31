246407ffa12928f2fe84aa01ce6d8b7b0092c897
Jason Less <jaless1997@gmail.com>
2017-05-31 03:24:59 -0700

    Create spaceship.v
    - Adding the spaceship implementation (before lasers)

---
+++ Lab_4/spaceship.v
@@ -0,0 +1,105 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    00:46:43 05/31/2017 
+// Design Name: 
+// Module Name:    spaceship 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module spaceship(
+	// Inputs
+	input wire clk,
+	input wire rst,
+	input wire button_left,
+	input wire button_right,
+	input wire button_center,
+	input wire [10:0] xCoord,
+	input wire [10:0] yCoord,
+	// Outputs
+	output wire [7:0] rgb,
+	output wire is_spaceship
+    );
+
+	///////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////
+	// Display screen boundaries
+   parameter LEFT_EDGE = 11'd0;
+   parameter RIGHT_EDGE = 11'd640;
+   parameter TOP_EDGE = 11'd0;
+   parameter BOTTOM_EDGE = 11'd480;
+
+	///////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////
+   // RGB Parameters [ BLUE | GREEN | RED ]
+	reg [7:0] set_color;
+	parameter COLOR_SPACESHIP = 8'b00111111;
+	parameter COLOR_ALIEN = 8'b10101010;
+	parameter COLOR_FLYING_SAUCER = 8'b10100111;
+	parameter COLOR_SPACE = 8'b00000000;
+	parameter COLOR_BLACK = 8'b00000000;
+	parameter COLOR_WHITE = 8'b11111111;
+	parameter COLOR_GREEN = 8'b00111000;
+	parameter COLOR_RED = 8'b00000111;
+	parameter COLOR_BLUE = 8'b11000000;
+	parameter COLOR_YELLOW = 8'b00111111;
+
+	// Spaceship Parameters
+	parameter SPACESHIP_HEIGHT = 11'd10;
+	parameter SPACESHIP_LENGTH = 11'd40;
+	parameter SPACESHIP_TOP = 11'd420;
+	parameter SPACESHIP_BOTTOM = 11'd430;
+	parameter SPACESHIP_INITIAL = 11'd320;
+	reg [10:0] spaceship_coord;
+		
+	// Position Updates
+   parameter MOVE_LEFT  = 11'd1;
+	parameter MOVE_RIGHT = 11'd1;
+		
+	// Initialize spaceship
+	initial begin
+		// Spaceship begins in the middle of the scren
+		spaceship_coord = SPACESHIP_INITIAL;
+	end
+
+	always @ (posedge clk) begin
+		if (rst) begin
+			// Reset position of the spaceship
+			spaceship_coord = SPACESHIP_INITIAL;
+		end
+		else begin
+			// Spaceship Controls
+			// Left button pressed, update spaceship position to the left (if possible)
+        	if (button_left && spaceship_coord > LEFT_EDGE + SPACESHIP_LENGTH / 2) begin
+				spaceship_coord = spaceship_coord - MOVE_LEFT;
+			end
+			// Right button pressed, update spaceship position to the right (if possible)
+         if (button_right && spaceship_coord < RIGHT_EDGE - SPACESHIP_LENGTH / 2) begin
+				spaceship_coord = spaceship_coord + MOVE_RIGHT;
+			end
+			// Update display of spaceship
+			if (yCoord >= SPACESHIP_TOP && yCoord <= SPACESHIP_BOTTOM && 
+				 xCoord >= spaceship_coord - SPACESHIP_LENGTH / 2 && xCoord <= spaceship_coord + SPACESHIP_LENGTH / 2
+				) begin
+				set_color <= COLOR_SPACESHIP;
+			end
+		end
+	end
+
+	assign rgb = set_color;
+	assign is_spaceship = (yCoord >= SPACESHIP_TOP && yCoord <= SPACESHIP_BOTTOM && 
+									xCoord >= spaceship_coord - SPACESHIP_LENGTH / 2 && xCoord <= spaceship_coord + SPACESHIP_LENGTH / 2
+									);
+	
+endmodule
