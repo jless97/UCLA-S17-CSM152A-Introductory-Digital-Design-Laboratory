f380c2d79f22b91a2389b1126dea1f12a48a06ce
Jason Less <jaless1997@gmail.com>
2017-06-01 08:55:35 -0700

    Update gameover_screen.v
    - Updated gameover screen: it works now
    - TODO: write gameover, print out score (right now temporary: space)

--- Lab_4/gameover_screen.v
+++ Lab_4/gameover_screen.v
@@ -38,8 +38,88 @@ module gameover_screen(
 
 	always @ (posedge clk) begin
 		if (yCoord >= 0 && yCoord < 480) begin
-			// Temporary (eventually, should say gameover, and display score)
-			set_color <= COLOR_WHITE;
+			// SPACE (40 by 50)
+			// Letter S (revised)
+			if ((xCoord > 0 && xCoord < 10) || (xCoord > 640-10 && xCoord < 640)) begin
+				set_color <= COLOR_WHITE;
+			end
+			else if (xCoord > 200 && xCoord < 240 && yCoord > 100 && yCoord < 150) begin
+				if (
+					(xCoord > 200 && xCoord < 210 && yCoord > 100 && yCoord < 105) ||
+					(xCoord > 230 && xCoord < 240 && yCoord > 100 && yCoord < 105) ||
+					(xCoord > 210 && xCoord < 230 && yCoord > 110 && yCoord < 120) ||
+					(xCoord >= 230 && xCoord < 240 && yCoord >= 115 && yCoord < 125) ||
+					(xCoord > 200 && xCoord < 210 && yCoord > 125 && yCoord < 135) ||
+					(xCoord >= 210 && xCoord < 230 && yCoord > 130 && yCoord < 140) ||
+					(xCoord > 200 && xCoord < 210 && yCoord > 145 && yCoord < 150) ||
+					(xCoord > 230 && xCoord < 240 && yCoord > 145 && yCoord < 150)
+					) begin
+					set_color <= COLOR_BLACK;
+				end
+				else begin
+					set_color <= COLOR_WHITE;
+				end
+			end
+			// Letter P (revised)
+			else if (xCoord > 250 && xCoord < 290 && yCoord > 100 && yCoord < 150) begin
+				if (
+					(xCoord > 285 && xCoord < 290 && yCoord > 100 && yCoord < 105) ||
+					(xCoord > 260 && xCoord < 280 && yCoord > 105 && yCoord < 120) ||
+					(xCoord > 285 && xCoord < 290 && yCoord > 120 && yCoord < 125) ||
+					(xCoord > 260 && xCoord < 290 && yCoord >= 125 && yCoord < 150)
+					) begin
+					set_color <= COLOR_BLACK;
+				end
+				else begin
+					set_color <= COLOR_WHITE;
+				end
+			end
+			// Letter A (revised)
+			else if (xCoord > 300 && xCoord < 340 && yCoord > 100 && yCoord < 150) begin
+				if (
+					(xCoord > 300 && xCoord < 305 && yCoord > 100 && yCoord < 105) ||
+					(xCoord > 335 && xCoord < 340 && yCoord > 100 && yCoord < 105) ||
+					(xCoord > 310 && xCoord < 330 && yCoord > 105 && yCoord < 120) ||
+					(xCoord > 310 && xCoord < 330 && yCoord > 125 && yCoord < 150)
+					) begin
+					set_color <= COLOR_BLACK;
+				end
+				else begin
+					set_color <= COLOR_WHITE;
+				end
+			end
+			// Letter C (revised)
+			else if (xCoord > 350 && xCoord < 390 && yCoord > 100 && yCoord < 150) begin
+				if (
+					(xCoord > 350 && xCoord < 360 && yCoord > 100 && yCoord < 105) ||
+					(xCoord > 380 && xCoord < 390 && yCoord > 100 && yCoord < 105) ||
+					(xCoord > 360 && xCoord < 380 && yCoord > 110 && yCoord < 140) ||
+					(xCoord >= 380 && xCoord < 390 && yCoord > 115 && yCoord < 135) ||
+					(xCoord > 350 && xCoord < 360 && yCoord > 145 && yCoord < 150) ||
+					(xCoord > 380 && xCoord < 390 && yCoord > 145 && yCoord < 150)
+					) begin
+					set_color <= COLOR_BLACK;
+				end
+				else begin
+					set_color <= COLOR_WHITE;
+				end
+			end
+			// Letter E (revised)
+			else if (xCoord > 400 && xCoord < 440 && yCoord > 100 && yCoord < 150) begin
+				if (
+					(xCoord > 410 && xCoord < 440 && yCoord > 110 && yCoord < 120) ||
+					(xCoord > 430 && xCoord < 440 && yCoord >= 120 && yCoord < 130) ||
+					(xCoord > 410 && xCoord < 440 && yCoord >= 130 && yCoord < 140)
+					) begin
+					set_color <= COLOR_BLACK;
+				end
+				else begin
+					set_color <= COLOR_WHITE;
+				end
+			end
+			else begin
+				set_color <= COLOR_BLACK;
+			end
 		end
 	end
 	
