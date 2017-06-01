f7156d3aaa14eac33122cfcd75795eeea296450e
Jason Less <jaless1997@gmail.com>
2017-05-31 17:33:46 -0700

    Update clk_div.v
    - updated other clocks

--- Lab_4/clk_div.v
+++ Lab_4/clk_div.v
@@ -20,22 +20,29 @@
 //////////////////////////////////////////////////////////////////////////////////
 module clk_div(
 	// Inputs
-	input wire clk,		//master clock: 50MHz
-	input wire rst,		//asynchronous reset
+	input wire clk,						//master clock: 50MHz
+	input wire rst,						//asynchronous reset
 	// Outputs
-	output wire dclk,		//pixel clock: 25MHz
-	output wire gameclk	//game clock: 1000Hz (Not using right now, not gameclk lol)
+	output wire dclk,						//pixel clock: 25MHz
+	output wire flying_saucer_clk,	//flying saucer clock: 1000Hz 
+	output wire alien_clk				//alien clock: 100Hz
 	);
 
 	// 1000 Hz clock
 	// 50,000 clock cycles for fasthz_clk to go high and return to low
 	// Thus, it takes half (i.e. 25,000) for gameclk to go high
-	reg gameclk_temp;
+	reg flying_saucer_clk_temp;
+	reg [31:0] flying_saucer_clk_count;
 
+	// 100 Hz clock
+	// 500,000 clock cycles for fasthz_clk to go high and return to low
+	// Thus, it takes half (i.e. 250,000) for gameclk to go high
+	reg alien_clk_temp;
+	reg [31:0] alien_clk_count;
+	
 	// 25 MHz Clock Implementation
 	// 17-bit counter variable
 	reg [17:0] q;
-	reg [31:0] gameclk_count;
 	
 	// Clock divider --
 	// Each bit in q is a clock signal that is
@@ -53,23 +60,38 @@ module clk_div(
 	// Game (1000 Hz) Clock Implementation
 	always @ (posedge clk or posedge rst) begin
 		if (rst == 1'b1) begin
-			gameclk_count <= 32'b0;
-         gameclk_temp <= 1'b0;
+			flying_saucer_clk_count <= 32'b0;
+         flying_saucer_clk_temp <= 1'b0;
       end
-      else if (gameclk_count == 32'd25000 - 32'b1) begin
-         gameclk_count <= 32'b0;
-         gameclk_temp <= ~gameclk;
+      else if (flying_saucer_clk_count == 32'd25000 - 32'b1) begin
+         flying_saucer_clk_count <= 32'b0;
+         flying_saucer_clk_temp <= ~flying_saucer_clk;
       end
       else begin
-         gameclk_count <= gameclk_count + 32'b1;
-         gameclk_temp <= gameclk;
+         flying_saucer_clk_count <= flying_saucer_clk_count + 32'b1;
+         flying_saucer_clk_temp <= flying_saucer_clk;
 		end
 	end
 	 
-	// 50Mhz ÷ 2^1 = 25MHz
+	// Game (100 Hz) Clock Implementation
+	always @ (posedge clk or posedge rst) begin
+		if (rst == 1'b1) begin
+			alien_clk_count <= 32'b0;
+         alien_clk_temp <= 1'b0;
+      end
+      else if (flying_saucer_clk_count == 32'd25000 - 32'b1) begin
+         alien_clk_count <= 32'b0;
+         alien_clk_temp <= ~alien_clk;
+      end
+      else begin
+         alien_clk_count <= alien_clk_count + 32'b1;
+         alien_clk_temp <= alien_clk;
+		end
+	end
+	 
+	// 50Mhz Ã· 2^1 = 25MHz
 	assign dclk = q[1];
-	assign gameclk = gameclk_temp;
-
-	// 
-
-endmodule
+	assign flying_saucer_clk = flying_saucer_clk_temp;
+	assign alien_clk = alien_clk_temp;
+ 
+endmodule 
