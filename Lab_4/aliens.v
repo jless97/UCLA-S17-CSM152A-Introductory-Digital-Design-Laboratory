35390159157afcfdb4ab0ac5c05bec6c150f7860
Jason Less <jaless1997@gmail.com>
2017-06-01 23:30:48 -0700

    Update aliens.v
    - Added two output wires: the x and y coordinates to be passed to the spaceship module for laser handling on spaceship side

--- Lab_4/aliens.v
+++ Lab_4/aliens.v
@@ -22,20 +22,21 @@ module aliens(
 	// Inputs
 	input wire clk,
 	input wire rst,
-	input wire restart,
 	input wire [1:0] mode,
 	input wire [10:0] xCoord,
 	input wire [10:0] yCoord,
 	input wire aliens,
+	// Trying to add aliens top
 	input wire [10:0] initial_xCoord,
 	input wire [10:0] initial_yCoord,
-	//input wire color,
 	input wire move_left,
 	input wire move_right,
 	input wire move_down,
 	// Outputs
 	output wire [7:0] rgb,
 	output wire is_alien,
+	output wire [10:0] current_xCoord,
+	output wire [10:0] current_yCoord,
 	output wire is_edge,
 	output wire is_bottom
 //	output wire is_hit
@@ -47,31 +48,17 @@ module aliens(
 
 	// RGB Parameters [ BLUE | GREEN | RED ]
 	reg [7:0] set_color;
-	// Row 1: purple
-	parameter COLOR_ALIEN_ROW_1 = 8'b11000111;
-	// Row 2: blue
-	parameter COLOR_ALIEN_ROW_2 = 8'b1100000;
-	// Row 3: red
-	parameter COLOR_ALIEN_ROW_3 = 8'b00001111;
-	// Row 4: green
-	parameter COLOR_ALIEN_ROW_4 = 8'b00111000;
-	// Row 5: Cyan
-	parameter COLOR_ALIEN_ROW_5 = 8'b10101010;
-	
+	parameter COLOR_ALIEN = 8'b10101010;
 
 		// Alien Parameters
 	parameter ALIEN_HEIGHT = 11'd16;
 	parameter ALIEN_LENGTH = 11'd30;
-		// TEMPORARY
-		parameter ALIEN_TOP = 11'd80;
-		parameter ALIEN_BOTTOM = 11'd96;
 	
 	// Alien registers
 	reg [10:0] alien_xCoord;
 	reg [10:0] alien_yCoord;
 	reg [10:0] alien_counter;
 	reg is_edge_temp;
-	reg is_bottom_temp;
 		
 	// Position Updates
 	parameter ALIEN_MOVE_LEFT = 11'd10;
@@ -80,44 +67,37 @@ module aliens(
 	
 	// Border (separation of objects) Parameters
 	parameter BARRIER_TOP = 11'd340;
-	parameter BARRIER_BOTTOM = 11'd400;
-
+	
 	// Initialize alien ships
-	reg temp;
 	initial begin
 		alien_xCoord = initial_xCoord;
 		alien_yCoord = initial_yCoord;
 		alien_counter = 0;
 		is_edge_temp = 0;
-		is_bottom_temp = 0;
-		temp = 0;
 	end
 
 	wire clk_frame = (xCoord == 0 && yCoord == 0);
 	always @ (posedge clk) begin
-		if (rst || mode == 0 || mode == 1 || restart) begin
+		if (rst || mode == 0 || mode == 1) begin
 			// TODO: When new objects added, reset their properties
 			// TODO: Reset screens (right now, just resets game level)
 			// Reset alien spaceship
 			alien_xCoord <= initial_xCoord;
 			alien_yCoord <= initial_yCoord;
-			is_bottom_temp <= 0;
-			//alien_counter <= 0;
-			temp <= 0;
 		end
 		if (clk_frame && mode == 2) begin
 			// Alien Controls
-			//if (temp) begin
+			// Moving left, update alien position to the left (if possible)
 			if (alien_counter >= 200) begin
 				alien_counter <= 0;
-				//is_bottom_temp <= 0;
-				// Moving down
+				// Moving down, update alien position downwards (if possible)
 				if (move_down) begin
 					// If at the bottom edge of the barriers, then game over
-					if (alien_yCoord >= BARRIER_TOP - ALIEN_HEIGHT / 2 - ALIEN_MOVE_DOWN) begin
-						// Subtract live from player (if lives == 0, gameover)
-						is_bottom_temp <= 1;
-						is_edge_temp <= 0;
+					if (alien_yCoord >= BARRIER_TOP - ALIEN_HEIGHT / 2) begin
+						// gameover
+						//is_start_screen = 1;
+						//is_switch_screen = 0;
+						//is_gameover_screen = 1;
 					end
 					// Normal move down
 					else begin
@@ -125,24 +105,23 @@ module aliens(
 						is_edge_temp <= 0;
 					end
 				end
-				// Moving left
 				else if (move_left && !is_edge) begin
 					// If at left edge of the display, bounce back
 					if (alien_xCoord <= LEFT_EDGE + ALIEN_LENGTH / 2 + 2*ALIEN_MOVE_LEFT ) begin
 						is_edge_temp <= 1;
-                  alien_xCoord <= alien_xCoord - ALIEN_MOVE_LEFT;
+                        alien_xCoord <= alien_xCoord - ALIEN_MOVE_LEFT;
 					end
 					//Normal left move
 					else begin
 						alien_xCoord <= alien_xCoord - ALIEN_MOVE_LEFT;
 					end
 				end
-				// Moving right
+				// Moving right, update alien position to the right (if possible)
 				else if (move_right && !is_edge) begin
 					// If at right edge of the display, bounce back
 					if (alien_xCoord >= RIGHT_EDGE - ALIEN_LENGTH / 2 - ALIEN_MOVE_RIGHT) begin
 						is_edge_temp <= 1;
-                  alien_xCoord <= alien_xCoord + ALIEN_MOVE_RIGHT;
+                        alien_xCoord <= alien_xCoord + ALIEN_MOVE_RIGHT;
 					end
 					// Normal right move
 					else begin
@@ -153,37 +132,23 @@ module aliens(
 			else begin
 				alien_counter <= alien_counter + 1;
 			end
-//			end
-//			else begin
-//				temp <= 1;
-//			end
 			// Update display of aliens
 			if (yCoord >= alien_yCoord - ALIEN_HEIGHT / 2 && yCoord <= alien_yCoord + ALIEN_HEIGHT / 2 &&
 				 xCoord >= alien_xCoord - ALIEN_LENGTH / 2 && xCoord <= alien_xCoord + ALIEN_LENGTH / 2
 				) begin
-				// Color scheme for different rows
-//				if (color == 0) begin
-//					set_color <= COLOR_ALIEN_ROW_1;
-//				end
-//				if (color == 1) begin
-//					set_color <= COLOR_ALIEN_ROW_2;
-//				end
-//				if (color == 2) begin
-//					set_color <= COLOR_ALIEN_ROW_3;
-//				end
-//				if (color == 3) begin
-//					set_color <= COLOR_ALIEN_ROW_4;
-//				end
-//				if (color == 4) begin
-//					set_color <= COLOR_ALIEN_ROW_5;
-//				end
-				set_color <= COLOR_ALIEN_ROW_5;
+				set_color <= COLOR_ALIEN;
 			end
 		end
 	end
 	
+	// Assign movement conditions
 	assign is_edge = is_edge_temp;
-	assign is_bottom = is_bottom_temp;
+	
+	// Assign coordinates (to be fed to the spaceship coordinate)
+	assign current_xCoord = alien_xCoord;
+	assign current_yCoord = alien_yCoord;
+	
+	// Assign color parameters
 	assign rgb = set_color;
 	assign is_alien = (yCoord >= alien_yCoord - ALIEN_HEIGHT / 2 && yCoord <= alien_yCoord + ALIEN_HEIGHT / 2 &&
 							 xCoord >= alien_xCoord - ALIEN_LENGTH / 2 && xCoord <= alien_xCoord + ALIEN_LENGTH / 2
